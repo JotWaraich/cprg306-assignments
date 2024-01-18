396f907ede82771de97adc46a35ecca4d51b9e6c
Jot Waraich <gurjagjot@gmail.com>
2024-01-18 13:26:35 -0700

    Gradiant background color added

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="text-center">
+    <div className="h-screen text-center bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500">
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <Link
         href="/week-2"
--- app/week-2/page.js
+++ app/week-2/page.js
@@ -2,7 +2,7 @@ import StudentInfo from "./student-info";
 
 export default function Page() {
   return (
-    <main className="text-center">
+    <main className="h-screen text-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
       <h1>Shopping List</h1>
       <StudentInfo />
     </main>
