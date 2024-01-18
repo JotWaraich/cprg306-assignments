02e808e66386db601abc134db64cede0bd0ea94f
Jot Waraich <gurjagjot@gmail.com>
2024-01-18 12:25:43 -0700

    First Commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <p>My name is Gurjagjot Singh Waraich</p>
+      <p>
+        my github repository link is:
+        https://github.com/JotWaraich/cprg306-assignments
+      </p>
+    </div>
+  );
+};
+
+export default StudentInfo;
