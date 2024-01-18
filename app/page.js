44d00909b107ab6d1c1001c51c58d68f72ddef3f
Jot Waraich <gurjagjot@gmail.com>
2024-01-18 12:36:03 -0700

    Finished

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
-import Page from "./week-2/page";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
+    <div className="text-center">
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Page />
+      <Link href="/week-2">
+        <button className="border border-blue-600 bg-blue-600 rounded-md">
+          Week 2
+        </button>
+      </Link>
     </div>
   );
 }
--- app/week-2/page.js
+++ app/week-2/page.js
@@ -2,7 +2,7 @@ import StudentInfo from "./student-info";
 
 export default function Page() {
   return (
-    <main>
+    <main className="text-center">
       <h1>Shopping List</h1>
       <StudentInfo />
     </main>
