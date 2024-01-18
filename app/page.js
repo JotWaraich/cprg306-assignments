c390df65b464864706556d7ccf7377d0b0b5fc0d
Jot Waraich <gurjagjot@gmail.com>
2024-01-18 14:10:04 -0700

    week 2 assinement dome

--- app/page.js
+++ app/page.js
@@ -2,14 +2,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="h-screen text-center bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500">
+    <div className="h-screen text-center bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500 flex flex-col gap-4">
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link
-        href="/week-2"
-        className="border border-blue-600 bg-blue-600 rounded-md"
-      >
-        Week 2
-      </Link>
+      <div>
+        <Link
+          href="/week-2"
+          className="w-44 border border-blue-600 bg-blue-600 rounded-md p-2"
+        >
+          Week 2
+        </Link>
+      </div>
     </div>
   );
 }
