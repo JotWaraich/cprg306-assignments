ed2f625cc8152f8e445100d1729ff5138d270014
Jot Waraich <gurjagjot@gmail.com>
2024-03-19 12:18:12 -0600

    google added

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -1,14 +1,23 @@
 "use client";
 
+import { BsGoogle } from "react-icons/bs";
 import { AiFillGithub } from "react-icons/ai";
 import { UseUserAuth } from "./_uttils/auth-context";
 import Page from "./shopping-list/page.js";
 export default function page() {
-  const { user, gitHubSignIn, firebaseSignOut } = UseUserAuth();
+  const { user, gitHubSignIn, firebaseSignOut, googleSignIn } = UseUserAuth();
 
   async function signInGithub() {
     try {
       await gitHubSignIn();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function signInGoogle() {
+    try {
+      await googleSignIn();
     } catch (error) {
       console.error(error);
     }
@@ -38,6 +47,13 @@ export default function page() {
             >
               <AiFillGithub className="mt-1 ml-4" />
               Sign in with GitHub
+            </button>
+            <button
+              className="text-white p-2 m-2 rounded-md bg-[#555555] flex flex-row w-80 gap-4 text-[25px]"
+              onClick={signInGoogle}
+            >
+              <BsGoogle className="mt-1 ml-4" />
+              Sign in with Google
             </button>
           </div>
         </div>
